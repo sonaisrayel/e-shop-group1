78b5885d17a17e92caedb8d01afc8889170d4ff7
sonaisrayel <sonai@brightcom.com>
2024-03-04 20:20:35 +0400

    Add routers to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import 'dotenv/config';
+const { PORT } = process.env;
 
 import express from 'express';
 const app = express();
 
 import { connection } from './storages/db.js';
-const { PORT } = process.env;
+
+import categoriesRouter from '../src/routes/category-router.js';
+import bucketsRouter from '../src/routes/bucket-router.js';
+import favoritesRouter from '../src/routes/favorites-router.js';
+import ordersRouter from '../src/routes/order-router.js';
+import productsRouter from '../src/routes/product-router.js';
+import usersRouter from '../src/routes/user-router.js';
+
+
+app.use('/category',categoriesRouter);
+app.use('/bucket',bucketsRouter);
+app.use('/favorites',favoritesRouter);
+app.use('/orders',ordersRouter);
+app.use('/products',productsRouter);
+app.use('/users',usersRouter);
 
 connection();
-
 
 
 app.listen(PORT, () => {
