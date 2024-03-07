fc78aca5190a8d645a15fcd416799bf40a693ce6
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2024-03-08 01:28:36 +0400

    Add createProduct

--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { createProduct } from '../controllers/product-controller.js';
 const router = Router();
+router.post('/', createProduct);
 
 export default router;
