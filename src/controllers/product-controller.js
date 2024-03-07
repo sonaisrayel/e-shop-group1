d3878a9597a07e9bc3590e8c9b7487cdd8efdc48
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2024-03-08 02:23:43 +0400

    Added getProduct by id

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,11 +1,11 @@
-import { Product } from "../models/product-model.js";
+import { Product } from '../models/product-model.js';
 export const createProduct = async (req, res) => {
     try {
         const { name, price, seller, description, category } = req.body;
         const newProduct = await Product.create({ name, price, seller, description, category });
         res.status(201).send({ data: newProduct });
     } catch (e) {
-        console.log(e.message)
+        console.log(e.message);
         throw new Error("Product isn't created");
     }
 };
@@ -14,12 +14,26 @@ export const getProducts = async (req, res) => {
     try {
         const products = await Product.find();
         if (products.length === 0) {
-            return res.status(404).send("Products not found");
+            return res.status(404).send('Products not found');
         }
- 
-        res.status(201).send({ data: products });
 
+        res.status(201).send({ data: products });
     } catch (e) {
-        throw new Error("Couldn't find products")
+        throw new Error("Couldn't find products");
     }
-}
+};
+
+export const getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            res.status(404).send('Product not found');
+            return;
+        }
+        res.status(201).send({ data: product });
+    } catch (e) {
+        console.log(e.message);
+        throw new Error('Something went wrong');
+    }
+};
