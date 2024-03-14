032c19d206c9c7920a8e5c1971de6b2a3267a2a7
Anush Asulyan <141956515+AnushAsulyan1718@users.noreply.github.com>
2024-03-14 22:17:29 +0400

    category-controller.js

---
+++ src/controllers/category-controller.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Category = require('./category-model');
+
+const app = express();
+const PORT = 3000; 
+
+app.use(express.json());
+
+connection()
+
+app.get('/categories/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+
+
+    if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+      return res.status(400).json({ error: 'Invalid category ID' });
+    }
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
