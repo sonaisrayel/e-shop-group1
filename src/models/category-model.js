8f987e8e7468fb9a929002efce50de28afcc6dec
Anush Asulyan <141956515+AnushAsulyan1718@users.noreply.github.com>
2024-03-14 22:40:32 +0400

    Update category-model.js

---
+++ src/models/category-model.js
@@ -0,0 +1,27 @@
+const bookCategories = [
+    { id:1, name:"Fiction" },
+    { id:2, name:"Science Fiction" },
+    { id:3, name:"Mystery" },
+    { id:4, name:"Thriller" },
+    { id:5, name:"Fantasy" },
+    { id:6, name:"Historical fiction"},
+    { id:7, name:"Adventure" },
+    { id:8, name:"Romance novel" },
+    { id:9, name:"Horror" },
+    { id:10, name:"Biography" },
+    { id:11, name:"History" },
+    { id:12, name:"Fairy tale" }
+  ];
+  
+  
+  const mongoose = require('mongoose');
+  
+  const categorySchema = new mongoose.Schema({
+    name:{type:String, 
+          isDiscounted:false, 
+          required:true},
+  });
+  
+  const Category = mongoose.model('Category', categorySchema);
+  
+  module.exports = Category;
