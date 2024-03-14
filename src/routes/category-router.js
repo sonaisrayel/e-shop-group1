aff5c672b1396664207c7677534e5a59d63d11df
Anush Asulyan <141956515+AnushAsulyan1718@users.noreply.github.com>
2024-03-14 22:44:09 +0400

    Update category-router.js

--- src/routes/category-router.js
+++ src/routes/category-router.js
@@ -1,4 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
+import { getCategory, getCategories, createCategory, updateCategory, deleteCategory } from '../controllers/category-controller.js';
+
+router.get('/', getCategories);
+router.get('/:id', getCategory);
+router.post('/', createCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
+
 export default router;
