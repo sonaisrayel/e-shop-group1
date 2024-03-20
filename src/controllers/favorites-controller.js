1f8ef8a4448791aeeb76d4e05a62086ec0115a8a
Meri Badoyan <badoyanmary@gmail.com>
2024-03-20 20:43:42 +0400

    some minor changes been made in favorites-controller

--- src/controllers/favorites-controller.js
+++ src/controllers/favorites-controller.js
@@ -1,10 +1,10 @@
 import { Favorites } from '../models/favorites-model.js';
 
 export const deleteFavorite = async (req, res) => {
-    const { productId } = req.params;
-    const userId = req.userInfo.id;
-
     try {
+        const { productId } = req.params;
+        const userId = req.userInfo.id;
+
         const favoriteData = await Favorites.findOneAndUpdate(
             { userId, products: productId },
             {
@@ -16,33 +16,33 @@ export const deleteFavorite = async (req, res) => {
         );
 
         if (!favoriteData) {
-            throw new Error('Item not found');
+            throw new Error('Product not found');
         }
 
-        res.status(201).send('ok');
+        res.status(201).send({ message: 'Product has been removed from Favorites' });
     } catch (error) {
-        res.status(404).send(error.message);
+        res.status(404).send({ message: e.message });
     }
 };
 
 export const getFavorites = async (req, res) => {
-    const { limit, skip } = req.query;
-    const userId = req.userInfo.id;
-
     try {
+        const { limit, skip } = req.query;
+        const userId = req.userInfo.id;
+
         const favorites = await Favorites.findOne({ userId }).limit(limit).skip(skip).populate('products');
 
-        res.status(201).send({ favorites });
+        res.status(201).send({ message: 'ok', data: favorites });
     } catch (e) {
-        res.status(404).send(e.message);
+        res.status(404).send({ message: e.message });
     }
 };
 
 export const addFavoriteProduct = async (req, res) => {
-    const { productId } = req.body;
-
-    const userId = req.userInfo.id;
     try {
+        const { productId } = req.body;
+        const userId = req.userInfo.id;
+
         const favoriteData = await Favorites.findOneAndUpdate(
             { userId },
             { $addToSet: { products: productId } },
@@ -53,8 +53,8 @@ export const addFavoriteProduct = async (req, res) => {
             throw new Error('Not found');
         }
 
-        res.status(201).send('added');
+        res.status(201).send({ message: 'Added to Favorites' });
     } catch (e) {
-        res.status(404).send(e.message);
+        res.status(404).send({ message: e.message });
     }
 };
