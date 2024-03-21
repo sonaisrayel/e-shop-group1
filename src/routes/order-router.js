8bc009310f584a1787112b729ae2959ad2fb83ec
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2024-03-22 01:56:59 +0400

    Implement createOrder functionality

--- src/routes/order-router.js
+++ src/routes/order-router.js
@@ -1,4 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 
+import { createOrder } from '../controllers/order-controller.js';
+router.post('/', createOrder)
 export default router;
+
