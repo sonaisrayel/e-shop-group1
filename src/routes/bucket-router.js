2aea1d47c685b614d18b9957cfea4c25cf181e3c
Meri Badoyan <badoyanmary@gmail.com>
2024-03-22 15:17:17 +0400

    Functionality for creating, adding, updating and deleting bucket and products in the bucket added

--- src/routes/bucket-router.js
+++ src/routes/bucket-router.js
@@ -1,4 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 
+import { getBucket, addProductToBucket, deleteProductFromBucket } from '../controllers/bucket-controller.js';
+
+router.get('/', getBucket);
+router.post('/', addProductToBucket);
+router.delete('/:productId', deleteProductFromBucket);
+
 export default router;
