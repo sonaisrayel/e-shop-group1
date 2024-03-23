32d1cdf223657e372dedc47ed5793896711bebfc
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2024-03-24 01:02:31 +0400

    Implement get user products functionality

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -3,8 +3,8 @@ const router = Router();
 
 import { upload } from '../libs/multer-lib.js';
 
-import { addUserImage, getUser, getUsers, updateUser } from '../controllers/user-controller.js';
-
+import { addUserImage, getUser, getUsers, updateUser, getUserProducts } from '../controllers/user-controller.js';
+router.get('/:id', getUserProducts);
 router.get('/', getUsers);
 router.get('/:id', getUser);
 router.patch('/', upload.single('file'), updateUser);
