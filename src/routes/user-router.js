8eecbc8922623e677267de013c42b969ec40c466
Meri Badoyan <badoyanmary@gmail.com>
2024-03-23 19:19:47 +0400

    isAdmin middleware added to user-router

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 const router = Router();
 
 import { upload } from '../libs/multer-lib.js';
+import { isAdmin } from '../middlewars/admin-middleware.js';
 
 import { addUserImage, getUser, getUsers, updateUser } from '../controllers/user-controller.js';
 
-router.get('/', getUsers);
+router.get('/', isAdmin, getUsers);
 router.get('/:id', getUser);
 router.patch('/', upload.single('file'), updateUser);
 router.post('/image', upload.single('file'), addUserImage);
