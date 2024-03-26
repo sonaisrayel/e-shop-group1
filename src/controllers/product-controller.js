a5e8990fb4de6464d44e168f04c4a5fab23dbf68
Lena Baghdasaryan <lenabaghdasaryan@mail.ru>
2024-03-27 00:52:40 +0400

    Improve update and delete functions

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,12 +1,15 @@
 import { Product } from '../models/product-model.js';
-
+import { productValidationSchema } from '../utils/validations.js';
 export const createProduct = async (req, res) => {
     try {
         const { userInfo } = req;
-        const { name, price, description, category, author, quantity } = req.body;
         if (!userInfo || userInfo.role !== 'seller') {
             throw new Error('Only sellers are authorized to create products');
         }
+        const { name, price, description, category, author, quantity } = await productValidationSchema.validateAsync(
+            req.body
+        );
+
         const newProduct = new Product({ name, price, seller: userInfo.id, quantity, description, category, author });
         await newProduct.save();
         res.status(201).send({ data: newProduct });
@@ -49,16 +52,13 @@ export const deleteProduct = async (req, res) => {
     try {
         const { userInfo } = req;
         const { id } = req.params;
-        const productToDelete = await Product.findById(id);
-        if (!productToDelete) {
-            throw new Error('Product not found');
-        }
-        if (!productToDelete.seller.equals(userInfo.id)) {
-            throw new Error('You are not authorized to delete this product');
-        }
-        const deletedProduct = await Product.findOneAndDelete({ _id: id });
+        const productToDelete = await Product.findOneAndDelete({ _id: id, seller: userInfo.id });
 
-        res.status(200).send({ data: deletedProduct });
+        if (!productToDelete) {
+            throw new Error('Product not found or unauthorized to delete');
+        }
+
+        res.status(200).send({ data: productToDelete });
     } catch (e) {
         res.status(404).send({ message: e.message });
     }
@@ -69,16 +69,14 @@ export const updateProduct = async (req, res) => {
         const { userInfo } = req;
         const { id } = req.params;
         const payload = req.body;
-        const productToUpdate = await Product.findOne({ _id: id });
+        const productToUpdate = await Product.findOneAndUpdate({ _id: id, seller: userInfo.id }, payload, {
+            new: true,
+        });
         if (!productToUpdate) {
-            throw new Error('Product not found');
+            throw new Error('Product not found or unauthorized to update');
         }
-        if (!productToUpdate.seller.equals(userInfo.id)) {
-            throw new Error('You are not authorized to update this product');
-        }
-        const updatedProduct = await Product.findOneAndUpdate({ _id: id }, payload, { new: true });
 
-        res.status(200).send({ data: updatedProduct });
+        res.status(200).send({ data: productToUpdate });
     } catch (e) {
         res.status(404).send(e.message);
     }
