0a42ea92d49079404931abf5a4cd0fb115865bc1
Meri Badoyan <badoyanmary@gmail.com>
2024-03-29 10:32:37 +0400

    admin controller updated

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,20 +1,21 @@
 import 'dotenv/config';
 import JWT from '../libs/jwt-lib.js';
+import ResponseHandler from '../responses/responseHandler.js';
 
 const { ADMIN_USERNAME, ADMIN_PASSWORD } = process.env;
 
-export const adminLogin = async (req, res) => {
+export const adminLogin = async (req, res, next) => {
     try {
         const { username, password } = req.body;
 
         if (ADMIN_USERNAME !== username || password !== ADMIN_PASSWORD) {
-            throw new Error('You are not an admin!!!');
+            return ResponseHandler.handleAuthorizationError(res, { message: 'You are not an admin!!!' });
         }
 
         const token = await JWT.createToken({ username, role: 'admin' });
 
-        res.status(200).send({ message: 'ok', token });
-    } catch (error) {
-        res.status(404).send({ message: error.message });
+        return ResponseHandler.handlePostResponse(res, { token });
+    } catch (err) {
+        next(err.message);
     }
 };
