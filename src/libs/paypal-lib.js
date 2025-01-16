4e5f3455d8aa235a4f2f04b4f88c1613c0084d1e
sonaisrayel <sonai@brightcom.com>
2025-01-16 14:07:02 +0400

    Test revert

--- src/libs/paypal-lib.js
+++ src/libs/paypal-lib.js
@@ -45,45 +45,6 @@ const pay = async (bucket) => {
     });
 };
 
-const pay1 = async (bucket) => {
-    const create_payment_json = {
-        intent: 'sale',
-        payer: {
-            payment_method: 'paypal',
-        },
-        redirect_urls: {
-            return_url: `${HOST}/${PORT}/bucket`,
-            cancel_url: `${HOST}/${PORT}/bucket`,
-        },
-        transactions: [
-            {
-                item_list: {
-                    items: bucket.products,
-                },
-                amount: {
-                    currency: 'USD',
-                    total: bucket.totalPrice,
-                },
-                description: 'This is the payment description.',
-            },
-        ],
-    };
-
-    paypal.payment.create(create_payment_json, (payment) => {
-        try {
-            // what is this for?
-            for (let i = 0; i < payment.links.length; i++) {
-                if (payment.links[i].rel === 'approval_url') {
-                    res.redirect(payment.links[i].href);
-                }
-            }
-        } catch (error) {
-            throw error;
-        }
-    });
-};
-
-
 const success = async (PayerID, paymentId) => {
     const execute_payment_json = {
         payer_id: PayerID,
